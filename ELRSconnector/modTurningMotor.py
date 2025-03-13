04f5800f45fa264094746d5bb7d69f24ce3205c2
Ravenix12 <shivani_mariappan@mymail.sutd.edu.sg>
2025-03-13 18:25:40 +0800

    added limits for motor 1

--- ELRSconnector/modTurningMotor.py
+++ ELRSconnector/modTurningMotor.py
@@ -7,19 +7,33 @@ import time
 motor = Motor(forward=17, backward=18)
 pwm = PWMOutputDevice(27)
 
+position = 100
+MIN_POS = 0
+MAX_POS = 1200
 current = None  # Tracks last received data to avoid redundant actions
 
 def move_forward():
     """Starts moving forward."""
-    print("Moving Forward...")
-    pwm.value = 0.5
-    motor.forward()
+    global position
+    if position < MAX_POS:
+        print("Moving Forward...")
+        pwm.value = 0.5
+        motor.forward()
+        position += 1 
+    else:
+        stop_motor()
 
 def move_backward():
     """Starts moving backward."""
-    print("Moving Backward...")
-    pwm.value = 0.5
-    motor.backward()
+    global position
+    if position > MIN_POS:
+        print("Moving Backward...")
+        pwm.value = 0.5
+        motor.backward()
+        position -= 1
+        time.sleep(0.02)
+    else:
+        stop_motor()
 
 def stop_motor():
     """Stops the motor."""
